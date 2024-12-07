e449913ed2c2502c5a5618e4f5b37c4fbe618619
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-12-07 13:02:22 +0000

    Update Schools.tsx

--- src/components/Schools.tsx
+++ src/components/Schools.tsx
@@ -1,12 +1,101 @@
 import React from 'react'
+import Image from "next/image";
+import logo0 from "../app/assets/logo0.jpg"
+import logo1 from "../app/assets/logo1.jpg"
+import logo2 from "../app/assets/logo2.jpg"
+import logo3 from "../app/assets/logo3.jpg"
+import logo4 from "../app/assets/logo4.jpg"
+import logo5 from "../app/assets/logo5.jpg"
+import logo6 from "../app/assets/logo6.jpg"
+import logo7 from "../app/assets/logo7.jpg"
+import logo8 from "../app/assets/logo8.jpg"
 
 const Schools = () => {
   return (
     <div >
           <div className="z-40 bg-[#000409] pb-[20rem] ">
             <h1 className="text-white text-center text-sm mt-10">
-                Trusted by 300+ educational institutions
+                Trusted by 150+ educational institutions
             </h1>
+
+
+            <div className='flex place-content-center logo m-a marquee' >
+                <div className='logo-container  flex animate'>
+                    <Image
+                        src={logo0}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo1}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo2}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo3}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo4}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo5}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo6}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo7}
+                            alt="star_image"
+                            width="100"
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+                    <Image
+                        src={logo8}
+                            alt="star_image"
+                            width="100" 
+                            height="100"
+                            sizes="fit-content"
+                            className=" rounded-full md:h-[5.5rem] h-[4rem] md:w-[5.5rem] w-[4rem] fit-content m-3"    
+                        />   
+        </div>
+
+        </div>
         </div>
 
 
