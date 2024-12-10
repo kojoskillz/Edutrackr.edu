94d231f6a704a72d452f86548f76d795b0d5bd17
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-12-10 07:51:02 +0000

    Update FAQ.tsx

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -4,18 +4,35 @@ import {
     AccordionItem,
     AccordionTrigger,
   } from "../components/ui/accordion"
-  
+  import Image from "next/image";
+  import questionmark from "../app/assets/question.png"
+  import checkmark from "../app/assets/checkmark.png"
+
+
   export default function AccordionDemo() {
     return (
 
         <div className="mt-32 mb-20">
+            <div className="flex relative m-auto border-blue-500 border-[2px] rounded-full w-[5.5rem] place-content-center m-auto ">
+                    <Image
+                    src={questionmark}
+                        alt="star_image"
+                        width="100"
+                        height="100"
+                        sizes="fit-content"
+                        className="h-4 w-5 mt-1 fit-content"    
+                    />
+                   <h1 className="text-white sticky font-semibold text-center lawn_color ">
+                      FAQ
+                    </h1>
+            </div>
           <div>
               <h1 className="text-white text-center  text-wrap font-bold md:text-6xl text-4xl ">
-                  Got Questions?<br /> We&apos;ve got <span className="lawn_color">Answers </span> 
+                  Got Questions?<br /> We&apos;ve Got <span className="lawn_color">Answers </span> 
               </h1>
           </div>
-      
-      <Accordion type="single" collapsible className="mt-10  w-[23rem] md:w-[45rem] lg:w-[45rem] m-auto place-content-center">
+      {/* FAQ for edutrack */}
+      <Accordion type="single" collapsible className="mt-10  w-[23 rem] md:w-[45rem] lg:w-[45rem] m-auto place-content-center">
       <AccordionItem value="item-1" className="text-white text-left border-[1px] border-white/5 px-4  glass2 rounded-[12px] ">
           <AccordionTrigger>What is a Student Management System?</AccordionTrigger>
           <AccordionContent>
@@ -53,6 +70,59 @@ import {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+
+{/* marquee for edutrack */}
+    <div className='flex place-content-center mt-20 gap-20 logo m-a marquee'>
+
+        <div className='logo-container gap-10 flex animate' >
+          
+            <Image
+                      src={checkmark}
+                          alt="star_image"
+                          width="100"
+                          height="100"
+                          sizes="fit-content"
+                          className="h-10 w-10 mt-1 fit-content"    
+                      />
+                    <h1 className="text-white text-5xl ">
+                      Try Edutrack
+                    </h1> 
+
+        </div>
+    
+        <div className='logo-container  gap-10 flex animate' >
+            <Image
+                      src={checkmark}
+                          alt="star_image"
+                          width="100"
+                          height="100"
+                          sizes="fit-content"
+                          className="h-10 w-10 mt-1 fit-content"    
+                      />
+                    <h1 className="text-white text-5xl ">
+                      Try Edutrack
+                    </h1> 
+
+        </div>
+
+        <div className='logo-container  gap-10 flex animate' >
+            <Image
+                      src={checkmark}
+                          alt="star_image"
+                          width="100"
+                          height="100"
+                          sizes="fit-content"
+                          className="h-10 w-10 mt-1 fit-content"    
+                      />
+                    <h1 className="text-white text-5xl ">
+                      Try Edutrack
+                    </h1> 
+
+        </div>
+
+    </div>
+
+
       </div>
     )
   }
