f880e2227d94d16efbdb5b2e8f9e7183f4167f48
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-08-01 21:29:56 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,28 @@
 import type { Metadata } from "next";
+import { ReactNode } from "react";
 import localFont from "next/font/local";
-// import { Agdasima} from 'next/font/google';
-import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-// const agdasima = Agdasima({
-//   subsets: ["latin"],
-//   variable: "--font-agdasima",
-//   weight: "400",
-// });
 
 export const metadata: Metadata = {
   title: "Edutrackr",
   description: "School Management System",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
