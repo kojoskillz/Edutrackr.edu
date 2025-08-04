42adc7090cce904a54f7496013104536cf5b3f56
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-08-04 09:52:44 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,6 @@ export default function landingPage() {
         <MagicCardDemo />
         <AccordionDemo />
         <LampDemo />
-    
-        {/* <WorldMapDemo /> */}
          
     </>
   );
